6816d9402274f9905a8e13ac7feab3248c76fc66
Crowdbotics <team@crowdbotics.com>
2021-09-22 15:00:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "blue_wood_30747Identifier",
+  url: "https://blue_wood_30747Identifier.botics.co",
+  api: "https://blue_wood_30747Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
